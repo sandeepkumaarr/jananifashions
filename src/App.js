5bf6f45439a6839c2dfb678c4a6682901b3d59a1
Sandeep Kumar <SandeepKumarr@protonmail.com>
2019-09-28 20:00:25 +0530

    updated menu-items withRouter higher order components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const NewArrivals = () => (
+  <div>
+    <h1>New Arrivals</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/newarrivals" component={NewArrivals} />
+    </Switch>  
     </div>
   );
 }
--- src/Components/directory/directory.component.jsx
+++ src/Components/directory/directory.component.jsx
@@ -10,29 +10,34 @@ class Directory extends React.Component {
                 {
                     title: 'New Arrivals',
                     imageUrl:'https://i.ibb.co/FWbS0BQ/New-Arrivals.jpg',
-                    id:'1'
+                    id:'1',
+                    linkUrl:'newarrivals'
                 },
                 {
                     title: 'Clothings',
                     imageUrl:'https://i.ibb.co/B6nP8yQ/Clothing.jpg',
-                    id:'2'
+                    id:'2',
+                    linkUrl:''
                 },
                 {
                     title: 'Footwears',
                     imageUrl:'https://i.ibb.co/m4KMsHZ/Footwears.jpg',
-                    id:'3'
+                    id:'3',
+                    linkUrl:''
                 },
                 {
                     title: 'Watches',
                     imageUrl:'https://i.ibb.co/RDCrrf2/watches.jpg',
                     size:'large',
-                    id:'4'
+                    id:'4',
+                    linkUrl:''
                 },
                 {
                     title: 'Terracotta Jewellery',
                     imageUrl:'https://i.ibb.co/Bn2t04y/Handmade-Terracotta-Jewellery.png',
                     size:'large',
-                    id:'5'
+                    id:'5',
+                    linkUrl:''
                 },
             ]
         }
@@ -42,8 +47,8 @@ class Directory extends React.Component {
        return( 
                 <div className = "directory-menu">
                     {
-                        this.state.sections.map(({title,imageUrl,id,size}) => (
-                            <MenuItem id={id} title={title} imageUrl = {imageUrl} size={size} />
+                        this.state.sections.map(({title,imageUrl,id,size, linkUrl}) => (
+                            <MenuItem key={id} title={title} imageUrl = {imageUrl} size={size} linkUrl={linkUrl} />
                         ))
                     }
                 </div>
